cc070528243759068244071bd0ea25f807b236dc
Rifat Khan <rifat6633@gmail.com>
2024-03-22 15:49:43 +0600

    add: padding

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,7 @@ import { ModeToggle } from "@/components/client-components";
 
 export default function Navbar() {
   return (
-    <header className="container flex justify-between items-center">
+    <header className="container flex justify-between items-center py-10">
       <div></div>
 
       <ModeToggle />

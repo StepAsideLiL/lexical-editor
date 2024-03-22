a050ccaccb80fb079cb858893b1205b3750694ef
Rifat Khan <rifat6633@gmail.com>
2024-03-22 15:49:12 +0600

    create: Navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,11 @@
+import { ModeToggle } from "@/components/client-components";
+
+export default function Navbar() {
+  return (
+    <header className="container flex justify-between items-center">
+      <div></div>
+
+      <ModeToggle />
+    </header>
+  );
+}

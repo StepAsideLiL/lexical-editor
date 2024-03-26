005e9710ae8c3ce50f3cd5fcdefd6481d98e2679
Rifat Khan <rifat6633@gmail.com>
2024-03-26 15:06:20 +0600

    update: component name and file name

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 // import EditorV1 from "@/components/editors/editor-v1";
-import Editor from "@/components/lexical-editor/editor";
+import LexicalEditor from "@/components/lexical-editor/lexical-editor";
 
 export default function Page() {
   return (
     <main className="container">
-      <Editor />
+      <LexicalEditor />
     </main>
   );
 }

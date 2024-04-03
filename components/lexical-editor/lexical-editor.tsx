24e8f816490b2e58fe3c5764a80625db73fc64c0
Rifat Khan <rifat6633@gmail.com>
2024-04-03 19:49:11 +0600

    add: pass root prop to with is editor content

--- components/lexical-editor/lexical-editor.tsx
+++ components/lexical-editor/lexical-editor.tsx
@@ -2,20 +2,28 @@
 
 import {
   InitialConfigType,
+  InitialEditorStateType,
   LexicalComposer,
 } from "@lexical/react/LexicalComposer";
 import Editor from "@/components/lexical-editor/editor";
 import { Nodes } from "@/components/lexical-editor/nodes";
 import { theme } from "@/components/lexical-editor/theme";
-import { EditorState } from "lexical";
+import {
+  EditorState,
+  SerializedEditorState,
+  SerializedLexicalNode,
+} from "lexical";
 import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 
 export default function LexicalEditor({
   setEditorState,
+  root,
 }: {
   setEditorState: (state: EditorState) => void;
+  root?: string;
 }) {
   const initialConfig: InitialConfigType = {
+    editorState: root,
     namespace: "Lexical-Editor",
     onError: (error: Error) => {
       throw error;

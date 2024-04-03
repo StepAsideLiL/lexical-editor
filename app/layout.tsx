008b95bfeed8be8b35692e4e59667e7c06c5a889
Rifat Khan <rifat6633@gmail.com>
2024-04-03 10:24:19 +0600

    add: Toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/navbar";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,8 @@ export default function RootLayout({
           <Navbar />
 
           {children}
+
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>

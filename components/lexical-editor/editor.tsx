5494e4487ab2786817cd067d295096008245c767
Rifat Khan <rifat6633@gmail.com>
2024-05-08 19:12:18 +0600

    remove: unnecessary code

--- components/lexical-editor/editor.tsx
+++ components/lexical-editor/editor.tsx
@@ -1,28 +1,18 @@
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
-import React, { useState } from "react";
+import React from "react";
 import MarkdownPlugin from "@/components/lexical-editor/plugins/MarkdownPlugin";
 import { ListPlugin } from "@lexical/react/LexicalListPlugin";
 import ListMaxIndentLevelPlugin from "@/components/lexical-editor/plugins/ListMaxIndentLevelPlugin";
 import { TabIndentationPlugin } from "@lexical/react/LexicalTabIndentationPlugin";
 
 export default function Editor() {
-  const [floatingAnchorElement, setFloatingAnchorElement] = useState<
-    HTMLDivElement | undefined
-  >(undefined);
-
-  const onRef = (_floatingAnchorElem: HTMLDivElement) => {
-    if (_floatingAnchorElem !== null) {
-      setFloatingAnchorElement(_floatingAnchorElem);
-    }
-  };
-
   return (
     <div className="relative">
       <RichTextPlugin
         contentEditable={
-          <div ref={onRef}>
+          <div>
             <ContentEditable className="h-96 focus-visible:outline-none" />
           </div>
         }

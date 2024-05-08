bd4377882c3ac45bf2a3a1521011c61311f95d64
Rifat Khan <rifat6633@gmail.com>
2024-05-08 23:07:05 +0600

    add: ToolBarPlugin to lexical editor

--- components/lexical-editor/editor.tsx
+++ components/lexical-editor/editor.tsx
@@ -6,23 +6,27 @@ import MarkdownPlugin from "@/components/lexical-editor/plugins/MarkdownPlugin";
 import { ListPlugin } from "@lexical/react/LexicalListPlugin";
 import ListMaxIndentLevelPlugin from "@/components/lexical-editor/plugins/ListMaxIndentLevelPlugin";
 import { TabIndentationPlugin } from "@lexical/react/LexicalTabIndentationPlugin";
+import ToolBarPlugin from "@/components/lexical-editor/plugins/ToolBarPlugin";
 
 export default function Editor() {
   return (
-    <div className="relative">
-      <RichTextPlugin
-        contentEditable={
-          <div>
-            <ContentEditable className="h-96 focus-visible:outline-none" />
-          </div>
-        }
-        placeholder={
-          <div className="absolute top-0 -z-50 text-muted-foreground">
-            Write...
-          </div>
-        }
-        ErrorBoundary={LexicalErrorBoundary}
-      />
+    <div>
+      <ToolBarPlugin />
+      <div className="relative">
+        <RichTextPlugin
+          contentEditable={
+            <div>
+              <ContentEditable className="h-96 focus-visible:outline-none py-5" />
+            </div>
+          }
+          placeholder={
+            <div className="absolute top-0 -z-50 text-muted-foreground py-5">
+              Write...
+            </div>
+          }
+          ErrorBoundary={LexicalErrorBoundary}
+        />
+      </div>
       <MarkdownPlugin />
       <ListPlugin />
       <ListMaxIndentLevelPlugin />

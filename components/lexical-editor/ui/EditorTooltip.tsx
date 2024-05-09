dae88f8e3852afcbe0a85b6a0a771501ae489842
Rifat Khan <rifat6633@gmail.com>
2024-05-09 16:46:25 +0600

    refactor: move uis and utils in individual files and folder

---
+++ components/lexical-editor/ui/EditorTooltip.tsx
@@ -0,0 +1,26 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+export function EditorTooltip({
+  children,
+  label,
+}: {
+  children?: React.ReactNode;
+  label?: string;
+}) {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+
+        <TooltipContent>
+          <p>{label}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
